9afb6e19fda77756763435c157d2af51c06b7a56
Harun-AR-Rashid <mdharunarrashid1998@gmail.com>
2024-09-07 14:01:01 +0600

    sum upto 100 done

--- sum-upto-100.js
+++ sum-upto-100.js
@@ -1,3 +1,16 @@
 /* 
 Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
 */
+let number = 1;
+let sum = 0;
+while (number <= 100) {
+    console.log(number);
+    sum = sum + number;
+    console.log(sum);
+    if (sum === 100 || sum >= 100){
+        console.log(sum);
+        break;
+    }
+    number++;
+}
+console.log('Sum reaches it conditions',sum)
