a7c58c66350ab33f253f70e1c94d9b9cb33e396b
Harun-AR-Rashid <mdharunarrashid1998@gmail.com>
2024-09-07 11:05:34 +0600

    task done

--- skip-five.js
+++ skip-five.js
@@ -1,3 +1,11 @@
 /*
 display odd number from 55 to 85 and skip the numbers divisible by 5.
 */
+let number = 55;
+while(number <= 85){
+    // console.log(number);
+    if(number % 2 !==0 && number % 5 !==0){
+        console.log(number);
+    }
+    number++;
+}
