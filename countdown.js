6df272c6ac121cfb622f63c6ec381826cc7ba2d1
Harun-AR-Rashid <mdharunarrashid1998@gmail.com>
2024-09-07 10:58:19 +0600

    cointsown done

--- countdown.js
+++ countdown.js
@@ -3,6 +3,24 @@
 Implement a countdown timer that counts down from 21 to 15.
 
  */
+// let count = 60;
+// const timer = setInterval(function() {
+//   count--;
+//   console.log(count);
+//   if (count === 0) {
+//     clearInterval(timer);
+//     console.log("Time's up!");
+//   }
+// }, 1000)
 
 
+let roll = 89;
+const harun = setInterval(() => {
+    roll++;
+    console.log(roll);
+    if(roll ===100){
+        clearInterval(harun);
+        console.log('Lucky Harun and Halima')
+    }
+}, 1000);
 /*programming hero*/
